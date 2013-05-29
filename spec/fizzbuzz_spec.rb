7652f70ccc60cc242b8c5f5d1d65a308a1e3d88f
Hiroyuki MORITA <chiastolite.1980@gmail.com>
2013-05-29 16:46:44 +0900

    low coverage

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,16 +4,12 @@ describe 'FizzBuzz' do
   context 3 do
     it do
       3.fizz?.should be_true
-      3.buzz?.should be_false
-      3.fizzbuzz?.should be_false
     end
   end
 
   context 5 do
     it do
       5.fizz?.should be_false
-      5.buzz?.should be_true
-      5.fizzbuzz?.should be_false
     end
   end
 
@@ -21,8 +17,6 @@ describe 'FizzBuzz' do
     subject{15}
     it do
       15.fizz?.should be_false
-      15.buzz?.should be_false
-      15.fizzbuzz?.should be_true
     end
   end
 end
