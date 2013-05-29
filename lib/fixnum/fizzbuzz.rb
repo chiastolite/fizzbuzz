f54a075e0ff09ff26ddf768bd92f401724147728
Hiroyuki MORITA <chiastolite.1980@gmail.com>
2013-05-29 13:32:22 +0900

    implement logics

--- lib/fixnum/fizzbuzz.rb
+++ lib/fixnum/fizzbuzz.rb
@@ -1,13 +1,15 @@
 class Fixnum
 
   def fizz?
+    !fizzbuzz? && modulo(3).zero?
   end
 
   def buzz?
-
+    !fizzbuzz? && modulo(5).zero?
   end
 
   def fizzbuzz?
+    modulo(15).zero?
   end
   
 end
